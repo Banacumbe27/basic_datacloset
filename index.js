0c09e03bf6bc71d940a011a65da9228339e277ff
Banacumbe27 <mimicjacson@gmail.com>
2025-08-08 11:43:37 +0700

    add js file and package files

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+let users = [];
+
+app.post('/', (req, res) => {
+    res.send('Hello! API is running!');
+});
+
+app.post('/add', (req, res) => {
+    let available=!users.find(u=>u.username===req.body.username&&u.password===req.body.password);
+    if(available) {
+    users.push(req.body);
+    res.json({ success:true});
+    } else {
+        res.status(409).json({success:false})
+    }
+
+});
+
+app.post('/login/:username/:password', (req, res) => {
+    let u_info = users.find(u => u.username === req.params.username && u.password === req.params.password);
+    if (u_info) {
+        res.json({ success: true});
+    } else {
+        res.status(404).json({ success: false });
+    }
+});
+
+app.post('/remove/:username/:password', (req, res) => {
+    let index = users.findIndex(u => u.username === req.params.username && u.password === req.params.password);
+    if (index === -1) {
+        res.status(404).json({ success: false });
+    } else {
+        users.splice(index, 1);
+        res.json({ success: true });
+    }
+});
+
+app.post('/wipe', (req, res) => {
+    users = [];
+    res.json({ success: true });
+});
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+});
